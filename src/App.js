f398719317eb43bf11dba1ed4443d19e80b4a845
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-09 14:39:58 -0700

    Starting the project, thank God

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import logo from "./logo.svg";
-import { Button, styled } from "@mui/material/";
-import { Add, Settings } from "@mui/icons-material";
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
 
 function App() {
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "skyblue",
-    color: "#888",
-    margin: 5,
-    "&:hover": { backgroundColor: "lightblue" },
-    "&:disabled": { backgroundColor: "gray", color: "white" },
-  }));
-
   return (
     <div className="App">
-      <Button variant="text">Text</Button>
-      <Button startIcon={<Settings />} variant="contained">
-        Contained
-      </Button>
-      <Button color="secondary" endIcon={<Add />} variant="contained">
-        Outlined
-      </Button>
-      <BlueButton>My button</BlueButton>
+      <Navbar />
+      <Sidebar />
+      <Feed />
+      <Rightbar />
     </div>
   );
 }
