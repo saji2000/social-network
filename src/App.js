55b836006407ec7263485a5a69ca8d52388c92e3
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-08 22:23:35 -0700

    Trying mui buttons, icons and inline css, thank God

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button } from "@mui/material/";
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="text">Text</Button>
+      <Button startIcon={<Settings />} variant="contained">
+        Contained
+      </Button>
+      <Button endIcon={<Add />} variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Button
+        variant="contained"
+        disabled
+        sx={{
+          backgroundColor: "skyblue",
+          color: "#888",
+          margin: 5,
+          "&:hover": { backgroundColor: "lightblue" },
+          "&:disabled": { backgroundColor: "gray", color: "white" },
+        }}
+      >
+        My unique button
+      </Button>
     </div>
   );
 }
