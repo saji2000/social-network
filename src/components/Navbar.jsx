9f882be2b62657fa3ef0882f5f387381fbfc1c3a
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-11 14:43:25 -0700

    Working on the nav bar, thank God

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,13 @@
-import { AppBar, Toolbar, styled, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+} from "@mui/material";
+import { Public, Mail } from "@mui/icons-material";
 import React from "react";
 
 const StyledToolBar = styled(Toolbar)({
@@ -6,11 +15,32 @@ const StyledToolBar = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "30%",
+  height: "1.4em",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({}));
+
 const Navbar = () => {
   return (
     <AppBar position="sticky">
-      <StyledToolBar>
-        <Typography variant="h6">Navbar</Typography>
+      <StyledToolBar variant="dense">
+        <Typography variant="h7" sx={{ display: { xs: "none", sm: "block" } }}>
+          Social Network
+        </Typography>
+        <Public />
+        <Search>
+          <InputBase placeholder="Search..." sx={{ fontSize: "0.7em" }} />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="primary">
+            <Mail color="white" />
+          </Badge>
+        </Icons>
       </StyledToolBar>
     </AppBar>
   );
