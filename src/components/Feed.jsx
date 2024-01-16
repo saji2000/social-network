0e7d1c101ddb123df19a41a89ae16875a92b0ac5
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-16 09:54:18 -0700

    Working on the post, thank God

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,62 +1,40 @@
 import React from "react";
-import {
-  Box,
-  Card,
-  CardHeader,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Avatar,
-  IconButton,
-  Typography,
-  Checkbox,
-} from "@mui/material";
-
-import { MoreVert, Favorite, Share, FavoriteBorder } from "@mui/icons-material";
+import { Box, Avatar } from "@mui/material";
+import Post from "./Post";
 
 const Feed = () => {
   return (
     <Box flex={4} p={2}>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVert />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="20%"
-          image="https://www.tastingtable.com/img/gallery/classic-seafood-paella-recipe/l-intro-1640888240.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <Checkbox
-              icon={<FavoriteBorder />}
-              checkedIcon={<Favorite sx={{ color: "red" }} />}
-            />
-          </IconButton>
-          <IconButton aria-label="share">
-            <Share />
-          </IconButton>
-        </CardActions>
-      </Card>
+      <Post
+        title="React Supremacy"
+        date="January 16th, 2024"
+        src="https://www.openxcell.com/wp-content/uploads/2023/03/react-web-development.jpg"
+        avatar={<Avatar src={require("../images/Sajad.jpeg")} />}
+      >
+        React.js is a top pick for front-end development because it's super
+        flexible and fast. It lets developers break things down into reusable
+        chunks (components), making code easier to manage. Plus, React's virtual
+        DOM keeps things speedy by only updating what needs to change, so your
+        web apps feel snappy. It also has a massive community and tons of handy
+        tools, so you're never short on resources. All in all, React rocks for
+        building modern, efficient web apps!
+      </Post>
+      <Post
+        title="Shrimp and Chorizo Paella"
+        date="January 15th, 2024"
+        src="https://www.tastingtable.com/img/gallery/classic-seafood-paella-recipe/l-intro-1640888240.jpg"
+        avatar={
+          <Avatar src="https://img.freepik.com/free-vector/coloured-chefdesign_1152-72.jpg?w=1380&t=st=1705423313~exp=1705423913~hmac=c15aa866aa2aaf7b696da1de0fdfc316d9294c0b7d43435c3f708aa03a3d7942">
+            R
+          </Avatar>
+        }
+      >
+        This impressive paella is a perfect party dish and a fun meal to cook
+        together with your guests. Add 1 cup of frozen peas along with the
+        mussels, if you like.
+      </Post>
+
+      {/* <Post /> */}
     </Box>
   );
 };
---
+++ src/components/Post.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+  Checkbox,
+} from "@mui/material";
+
+import { MoreVert, Favorite, Share, FavoriteBorder } from "@mui/icons-material";
+
+const Post = ({ children, title, date, src, avatar }) => {
+  const styledAvatar = React.cloneElement(avatar, {
+    sx: { width: "2.5em", height: "2.5em" },
+  });
+  return (
+    <div>
+      <Card sx={{ margin: "1em" }}>
+        <CardHeader
+          avatar={styledAvatar}
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title={title}
+          subheader={date}
+        />
+        <CardMedia
+          component="img"
+          height="20%"
+          image={src}
+          alt="broken image"
+        />
+        <CardContent>
+          <Typography variant="body1" color="text.secondary">
+            {children}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox
+              icon={<FavoriteBorder />}
+              checkedIcon={<Favorite sx={{ color: "red" }} />}
+            />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default Post;
