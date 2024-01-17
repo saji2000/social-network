4692948091dfa40a0410dd168c46ac62b60d9f4f
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-16 18:57:59 -0700

    Used map for the posts, much cleaner now thank God

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -19,7 +19,7 @@ const posts = [
     avatarSrc:
       "https://img.freepik.com/free-vector/coloured-chefdesign_1152-72.jpg?w=1380&t=st=1705423313~exp=1705423913~hmac=c15aa866aa2aaf7b696da1de0fdfc316d9294c0b7d43435c3f708aa03a3d7942",
     description:
-      "React.js is a top pick for front-end development because it's superflexible and fast. It lets developers break things down into reusable chunks (components), making code easier to manage. Plus, React's virtual DOM keeps things speedy by only updating what needs to change, so your web apps feel snappy. It also has a massive community and tons of handy tools, so you're never short on resources. All in all, React rocks for building modern, efficient web apps!",
+      "Shrimp and Chorizo Paella is a vibrant Spanish rice dish that combines succulent shrimp and flavorful chorizo sausage with saffron-infused rice, creating a delicious and colorful meal.",
   },
 ];
 
