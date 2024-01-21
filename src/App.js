4a74a8fb3d4fb928714793357eaf68a829cc4280
saji2000 <sajaddaneshmand79@gmail.com>
2024-01-21 14:14:23 -0700

    Dark theme and project finished,thank God

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from "./logo.svg";
 import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Add from "./components/Add";
 import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [mode, setMode] = useState("dark");
+  const prefersDarkMode =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [mode, setMode] = useState(prefersDarkMode ? "dark" : "light");
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = (e) => {
+      setMode(e.matches ? "dark" : "light");
+    };
+
+    // Add event listener
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Remove event listener on cleanup
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
 
   const darkTheme = createTheme({
     palette: {
